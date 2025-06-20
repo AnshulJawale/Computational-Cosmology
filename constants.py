22149b5af1f3cc8a3e23bd04d8b3d1ff7d6b1292
Anshul Jawale <anshuljawale04@gmail.com>
2025-06-20 14:06:25 +0530

    Added CMB

--- constants.py
+++ constants.py
@@ -5,3 +5,7 @@ Gyr = 3.1556926e16                          #? Gyr to seconds
 PI = np.pi                                 #? Pi
 G = 6.67430e-11                            #? Gravitational constant in m^3 kg^-1 s^-2
 c = 299792458                              #? Speed of light in m/s
+h = 6.62607015e-34                         #? Planck's constant in J·s
+Ccm=2.99792458e10                        #? Speed of light in cm/s
+k_B = 1.380649e-23                         #? Boltzmann constant in J/K
+MJysrSI = 1.0e-23                               #? MJy/sr to SI units
